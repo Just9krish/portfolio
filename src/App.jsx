2c9210d272161245c30f4c5757d45f9e0a99745b
just9krish <rvamit2648@gmail.com>
2023-02-01 21:12:59 +0530

    base is complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/hero";
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/hero.jsx
@@ -0,0 +1,18 @@
+export default function index() {
+  return (
+    <section id="hero" class="jumbotron">
+      <div class="container">
+        <h1 class="hero-title load-hidden">
+          Hi, my name is <span class="text-color-main">Amit Vishwakarma</span>
+          <br />
+          I'm the FullStack Developer.
+        </h1>
+        <p class="hero-cta load-hidden">
+          <a rel="noreferrer" class="cta-btn cta-btn--hero" href="#about">
+            Know more
+          </a>
+        </p>
+      </div>
+    </section>
+  );
+}
