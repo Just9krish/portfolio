60f5176b194b677151ad492e8bd45de387a2b5a0
just9krish <rvamit2648@gmail.com>
2023-02-14 17:04:59 +0530

    changes name of component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Hero from "./components/Hero";
+import Hero from "./components/Herosection";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Blogs from "./components/Blogs";
