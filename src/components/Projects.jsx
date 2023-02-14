bbba2efded16f326ab9b8cab495b09114368d65a
just9krish <rvamit2648@gmail.com>
2023-02-14 16:38:15 +0530

    changed layout of project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,12 +4,10 @@ import { Data } from "../utils/ProjectsData";
 import Project from "./Project";
 
 export default function Projects() {
-  // console.log(Data);
-
   return (
     <section id="projects">
       <div className="container">
-        <div className="project-wrapper">
+        <div className="projects-wrapper">
           <h2 className="section-title dark-blue-text">Projects</h2>
           {Data.length > 0 ? (
             Data.map((obj) => <Project obj={obj} key={obj.id} />)
--- src/utils/ProjectsData.js
+++ src/utils/ProjectsData.js
@@ -11,13 +11,17 @@ export const Data = [
     live: "https://quizzicalapponline.netlify.app/",
     image: quizDesktop,
     id: 1,
+    brief:
+      "The Quizzical is a React-based web app that allows users to test their knowledge with a variety of different questions. Users can customize their quiz by specifying the number of questions they want to answer, the type of questions (e.g. multiple choice, true/false), and the level of difficulty. The app fetches all of the question data from the OpenDB API, ensuring that the questions are up-to-date and accurate. The user-friendly interface makes it easy for users to navigate and select their preferred quiz settings.",
   },
   {
-    name: "just9blogs: A Bloggin Website",
+    name: "Just9blogs: A Blog Website",
     repo: "https://github.com/Just9krish/Blogs-website",
     live: "https://quizzicalapponline.netlify.app/",
     image: quizDesktop,
     id: 2,
+    brief:
+      "Just9blogs is a blog website that allows users to read blog posts on various topics, but only the administrator can manage the content. The administrator can add, update, and delete posts using their unique login credentials. The website is user-friendly and accessible for all types of users looking for informative content.",
   },
   {
     name: "Find you country",
@@ -25,6 +29,8 @@ export const Data = [
     live: "https://findwhereintheworld.netlify.app/",
     image: restDesktop,
     id: 3,
+    brief:
+      "Find Your Country is a React-based web app that allows users to search for detailed information about any country in the world. Users can filter countries by region and access important data points like population, currency, and bordering countries. The app uses the Rest Countries API to fetch up-to-date and accurate information. Its clean and intuitive design makes it easy to use for anyone looking to learn more about countries around the world.",
   },
   {
     name: "Todo: A webapp",
@@ -32,19 +38,7 @@ export const Data = [
     live: "https://javascriptodoapp.netlify.app/",
     image: todoDesktop,
     id: 4,
-  },
-  {
-    name: "Tip calculator",
-    repo: "https://github.com/Just9krish/Tip-Calculator-App",
-    live: "https://tip-calculator-just9krish.netlify.app/",
-    image: tipCalDesktop,
-    id: 5,
-  },
-  {
-    name: "Space Tourism",
-    repo: "https://github.com/Just9krish/Space-Tourism-Website",
-    live: "https://book-your-space-tour.netlify.app/",
-    image: spaceDesktop,
-    id: 6,
+    brief:
+      "The Todo app is a JavaScript and SCSS-based task manager that allows users to easily create, organize, and prioritize their daily tasks. Users can mark tasks as complete, filter tasks by their completion status, and even drag and drop tasks to prioritize them. With its intuitive design and powerful features, the Todo app is the perfect tool for anyone looking to stay organized and manage their daily tasks effectively.",
   },
 ];
