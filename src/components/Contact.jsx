435806d53221139d0482fa6177617a3397a90aac
just9krish <rvamit2648@gmail.com>
2023-02-18 20:59:35 +0530

    modified

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,42 +5,47 @@ export default function Contact() {
     <section id="contact" className="contact">
       <div className="container">
         <div class="contact__wrapper">
-          <div className="contact__image-wrapper">
+          {/* <div className="contact__image-wrapper">
             <img src={contactImg} alt="" className="contact__image" />
-          </div>
+          </div> */}
           <div class="contact__form-wrapper">
-            <h2 className="section-title">Contact</h2>
+            <h2 className="contact__form-title section-title">
+              Drop Me a Message
+            </h2>
             <form class="contact__form" action="">
-              <div class="contact__form-group">
-                <input id="name" class="contact__input" type="text" required />
-                <label class="contact__label" for="name">
-                  Name
-                </label>
+              <div>
+                <div class="contact__form-group">
+                  <input
+                    id="name"
+                    class="contact__input"
+                    type="text"
+                    placeholder="Your Name *"
+                    required
+                  />
+                </div>
+                <div class="contact__form-group">
+                  <input
+                    id="email"
+                    class="contact__input"
+                    type="email"
+                    placeholder="You Email *"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="">
+                <div class="contact__form-group">
+                  <textarea
+                    id="message"
+                    class="contact__input textaria"
+                    type="text"
+                    placeholder="Your Message *"
+                    required
+                  />
+                </div>
               </div>
               <div class="contact__form-group">
-                <input
-                  id="email"
-                  class="contact__input"
-                  type="email"
-                  required
-                />
-                <label class="contact__label" for="email">
-                  Email
-                </label>
-              </div>
-              <div class="contact__form-group">
-                <input
-                  id="message"
-                  class="contact__input"
-                  type="text"
-                  required
-                />
-                <label class="contact__label" for="message">
-                  Message
-                </label>
-              </div>
-              <div class="contact__form-group">
-                <button class="cta-btn cta-btn--resume contact__btn">
+                <button class="cta-btn cta-btn--contact contact__btn">
                   Send
                 </button>
               </div>
