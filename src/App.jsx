b3ab2759e76fb15038c00eec96a767b6b87fa0e9
just9krish <rvamit2648@gmail.com>
2023-03-05 21:45:05 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Projects from "./components/Projects";
 import Blogs from "./components/Blogs";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Hero />
       <About />
       <Projects />
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <header class="header">
+      <div class="header__logo">Amit</div>
+      <div class="header__menu">
+        <nav class="menu">
+          <ul class="menu__list">
+            <li class="menu__item">Home</li>
+            <li class="menu__item">About</li>
+            <li class="menu__item">Skills</li>
+            <li class="menu__item">Projects</li>
+            <li class="menu__item">Blogs</li>
+            <li class="menu__item">Contacts</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
