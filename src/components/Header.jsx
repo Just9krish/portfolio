a8aebee872cdf67fd4f3a62139790c3a74268263
just9krish <rvamit2648@gmail.com>
2023-03-06 21:54:01 +0530

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,38 @@
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header class="header">
-      <div class="header__logo">Amit</div>
-      <div class="header__menu">
-        <nav class="menu">
-          <ul class="menu__list">
-            <li class="menu__item">Home</li>
-            <li class="menu__item">About</li>
-            <li class="menu__item">Skills</li>
-            <li class="menu__item">Projects</li>
-            <li class="menu__item">Blogs</li>
-            <li class="menu__item">Contacts</li>
+    <header className="header">
+      <div className="header__logo">Amit</div>
+      {!isOpen ? (
+        <button
+          className="header__menu-button"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      ) : (
+        <button
+          className="header__menu-button"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+      )}
+
+      <div className={`header__menu-items ${isOpen ? "active" : ""}`}>
+        <nav className="menu">
+          <ul className="menu__list">
+            <li className="menu__item">Home</li>
+            <li className="menu__item">About</li>
+            <li className="menu__item">Skills</li>
+            <li className="menu__item">Projects</li>
+            <li className="menu__item">Blogs</li>
+            <li className="menu__item">Contacts</li>
           </ul>
         </nav>
       </div>
--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,22 +1,25 @@
 export default function Herosection() {
   return (
-    <section id="hero">
+    <section className="hero" id="hero">
       <div className="container">
-        <h1 className="hero-title">
-          Hi, my name is
-          <span className="text-color-main"> Amit Vishwakarma</span>
+        <h1 className="hero__title">
+          Amit Vishwakarma
+          {/* Hi, my name is
+          <span className="hero__title-name text-color-main">
+            {" "}
+            Amit Vishwakarma
+          </span>
           <br />
-          I'm the Full Stack Developer.
+          I'm the Full Stack Developer. */}
         </h1>
-        <p className="hero-cta load-hidden">
-          <a
-            rel="noreferrer"
-            className="cta-btn cta-btn--hero"
-            href="#projects"
-          >
-            View my work
-          </a>
+        <p className="hero__discription">
+          I'm Full Stack Developer on a quest to constantly expand my skills and
+          push the boundaries of what's possible. Let's start scrolling and
+          learn more about me.
         </p>
+        <a rel="noreferrer" className="cta-btn cta-btn--hero" href="#projects">
+          View my work
+        </a>
       </div>
     </section>
   );
